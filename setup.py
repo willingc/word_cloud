a7717233e7fec3130a84db20d3f3215f7bd9bde6
Carol Willing <carolcode@willingconsulting.com>
2023-11-03 15:32:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import io
 from setuptools import setup
 from setuptools.extension import Extension
-import versioneer
+
 
 with io.open('README.md', encoding='utf_8') as fp:
     readme = fp.read()
@@ -10,8 +10,7 @@ setup(
     author="Andreas Mueller",
     author_email="t3kcit+wordcloud@gmail.com",
     name='wordcloud',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+
     url='https://github.com/amueller/word_cloud',
     description='A little word cloud generator',
     long_description=readme,
